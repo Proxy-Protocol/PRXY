78e9c466bbb821da5ba9fb59d23a108c62a42a54
shivam3012 <shivamverma3012@gmail.com>
2021-05-07 18:06:45 +0530

    dynamic start block

--- contracts/PRXY.sol
+++ contracts/PRXY.sol
@@ -18,9 +18,9 @@ contract ProxyCoin is ERC20Burnable, Ownable {
   event MintDurationChanged(uint256 oldValue, uint256 newValue);
   event MintCycleCapChanged(uint256 oldValue, uint256 newValue);
 
-  constructor (address _treasuryWallet) ERC20("Proxy Coin", "PRXY") {
+  constructor (address _treasuryWallet, uint256 _lastMintBlock) ERC20("Proxy Coin", "PRXY") {
     _setupDecimals(18);
-    lastMintBlock = block.number;
+    lastMintBlock = _lastMintBlock;
     treasuryWallet = _treasuryWallet;
     _mint(treasuryWallet, perBlockSupply);
     mintCycle++;

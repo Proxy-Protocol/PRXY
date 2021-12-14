4cedfbe3367ce8318f375e3eae87ea7c116a65e6
shivam3012 <shivamverma3012@gmail.com>
2021-12-15 02:50:46 +0530

    mint from treasury function

--- contracts/PRXY_V2.sol
+++ contracts/PRXY_V2.sol
@@ -3,14 +3,25 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract ProxyCoin_V2 is ERC20Burnable, Ownable {
+contract ProxyCoin_V2 is ERC20Burnable, Ownable, Pausable {
     mapping(address => bool) public isLocked;
+    mapping(address => bool) public whitelist;
 
     constructor(address _treasuryWallet, uint256 _amount)
         ERC20("Proxy", "PRXY")
     {
         _mint(_treasuryWallet, _amount);
+    }
+
+    function mintByWhitelisted(address addr, uint256 amount)
+        external
+        onlyOwner
+        whenNotPaused
+    {
+        require(whitelist[_msgSender()], "ProxyCoin_V2: Not whiteslisted");
+        _mint(addr, amount);
     }
 
     function mint(address addr, uint256 amount) external onlyOwner {
@@ -19,6 +30,14 @@ contract ProxyCoin_V2 is ERC20Burnable, Ownable {
 
     function burnByOwner(address addr, uint256 amount) external onlyOwner {
         _burn(addr, amount);
+    }
+
+    function addToWhitelist(address _address) public onlyOwner {
+        whitelist[_address] = true;
+    }
+
+    function removeFromWhitelist(address _address) external onlyOwner {
+        whitelist[_address] = false;
     }
 
     function lockTransfer(address _addr) external onlyOwner {
